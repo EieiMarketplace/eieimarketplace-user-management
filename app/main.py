c410bb03aa85a1e5aa7053f6779e4818aa2c6d46
pok1220 <pokkung1220@gmail.com>
2025-09-07 17:25:42 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .models import Role
 from .routers import user_router
-
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Eiei Marketplace User Management")
-
+list = ["http://localhost:5000",
+        "http://localhost:8000",
+        "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=list,       
+    allow_credentials=True,
+    allow_methods=["*"],          # GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],          # Authorization, Content-Type, etc.
+)
 # init default roles
 from sqlalchemy.orm import Session
 db = Session(bind=engine)
